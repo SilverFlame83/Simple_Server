3223875da62cadf9845b997da67640682f2b69de
Korman <you@example.com>
2021-05-29 22:15:55 +0300

    New changes

--- app.js
+++ app.js
@@ -7,7 +7,12 @@ app.set('view engine', 'ejs');
 app.listen(3000);
 
 app.get('/',(req,res)=>{
-    res.render('index',{title: 'Home'});
+    const blogs = [
+        {title: 'Find some examples with express', snippet:'Lorem ipsum dodlor sit ammet conseevdtor'},
+        {title: 'Let\'s start mastering your skils', snippet:'Lorem ipsum dodlor sit ammet conseevdtor'},
+        {title: 'How to defeat the browser', snippet:'Lorem ipsum dodlor sit ammet conseevdtor'}
+    ]
+    res.render('index',{title: 'Home', blogs});
 });
 
 app.get('/about',(req,res)=>{
