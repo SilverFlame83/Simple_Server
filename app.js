fd60a895753bb887cb2767fccdd924c33bdbc8f7
Korman <you@example.com>
2021-05-30 19:47:56 +0300

    Style change

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ app.set('view engine', 'ejs');
 
 app.listen(3000);
 
+app.use((req,res, next)=>{
+    console.log('new request made:');
+    console.log('host: ', req.hostname);
+    console.log('path: ', req.path);
+    console.log('method: ', req.method);
+    next();
+});
+
 app.get('/',(req,res)=>{
     const blogs = [
         {title: 'Find some examples with express', snippet:'Lorem ipsum dodlor sit ammet conseevdtor'},
